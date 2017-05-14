0998752301ca61e3512c81e8314aa5cea143c3d1
Antonio Teran <teran@mit.edu>
2017-05-14 19:03:48 -0400

    third one-second fix

--- scripts/deltaOdomCalc.py
+++ scripts/deltaOdomCalc.py
@@ -109,6 +109,8 @@ class deltaOdomCalc():
 			# deltas.y = 
 			# Testing ------
 			deltas.theta = eulAng_orientation[2] - self.pastPose[2]
+			deltas.x = deltas.x/math.cos(deltas.theta)
+			deltas.y = deltas.y/math.sin(deltas.theta)
 			self.deltas_publisher.publish(deltas)
 			estimatedPose = Pose2D()
 			for e in self.deltaHistory:
